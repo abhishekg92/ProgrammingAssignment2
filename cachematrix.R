e02e4109e2b5e2a647393342678bd6acec2f8a49
AbhisheK Gupta <abhishekgupta92@gmail.com>
2015-02-20 20:20:10 +0530

    Have updated CacheMatrix.R with the appropriate functions to retrieve the cache of the inverse matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# makeCacheMatrix: Purpose is to store a martix and a cached value of the inverse of the matrix.
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
 
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve #store the matrix
+  getmatrix<-function() m               #retrieve the matrix
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# function calculates the inverse of a special matrix created with makeCacheMatrix
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix() # if a cached value exists return it
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get() #otherwise store the value
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
